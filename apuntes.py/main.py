5bb55ea916ae4b6eccdb22d235a4847ad05552e1
oscaralvarezz <oscar.alvarezdodani@usp.ceu.es>
2022-10-26 17:19:38 +0200

    .
    .

--- apuntes.py/main.py
+++ apuntes.py/main.py
@@ -6,9 +6,9 @@ from vehiculo import lista
 from vehiculo import catalogar
 if __name__ == "__main__":
     catalogar(lista ,2)
-    #catalogar(lista, 4)
+    catalogar(lista, 4)
 #parece correcto
 
-
+#esto debemos de hacerlo con todos los ejercicios
 
 
